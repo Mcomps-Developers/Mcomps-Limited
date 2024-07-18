c24271da61dfbe59648f811b40a517c41be7decb
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-18 12:09:36 +0300

    Update home-component.blade.php
    Co-Authored-By: Muremi Peter <116870103+petermuremi351@users.noreply.github.com>

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -654,6 +654,49 @@
 
         </div>
     </section> --}}
+    <section class="mil-p-120-60">
+        <div class="mil-deco" style="top: 0; left: 25%;"></div>
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-lg-5">
+                    <div class="mil-h1">4<span class="mil-accent">+</span></div>
+                    <h6 class="mil-mb-60">Years Experience</h6>
+
+                    <h2 class="mil-mb-60">We Run All Kinds of IT Services That Vow Your Success</h2>
+                </div>
+                <div class="col-lg-6">
+
+                    <h3 class="mil-mb-60">Accelerate <span class="mil-accent">Innovation</span> with world class tech teams We’ll match you to an entire <span class="mil-accent">Remote Technology</span></h3>
+
+                    <div class="row">
+                        <div class="col-lg-6">
+
+                            <h6 class="mil-mb-30"><span class="mil-accent">10+</span>&nbsp; Proffesional Staff</h6>
+
+                            <div class="mil-divider mil-divider-left mil-mb-60"></div>
+
+                        </div>
+                        <div class="col-lg-6">
+
+                            <h6 class="mil-mb-30"><span class="mil-accent">1000+</span>&nbsp; Satisfied Clients</h6>
+
+                            <div class="mil-divider mil-divider-left mil-mb-60"></div>
+
+                        </div>
+
+                        <div class="col-lg-6">
+
+                            <h6 class="mil-mb-30"><span class="mil-accent">50+</span>&nbsp; Solutions Offered</h6>
+
+                            <div class="mil-divider mil-divider-left mil-mb-60"></div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
 
 
 </div>
